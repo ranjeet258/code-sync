317c0c32511a8f1eba207ef3d9fb2720c480fed4
RanJeet <ranjeetkrgupta258@gmail.com>
2026-02-03 23:39:47 +0530

    Update solution for rotate-image

--- leetcode/rotate-image.py
+++ leetcode/rotate-image.py
@@ -4,10 +4,7 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n=len(matrix)
-        new = [ [0] * n for i in range(n)]
-        for i in range(n):
-            for j in range(n):
-                new[j][n-1-i]=matrix[i][j]
-        for i in range(n):
-            for j in range(n):
-                matrix[i][j]=new[i][j]
+        matrix.reverse()
+        for i in  range(n):
+            for j in range(i,n):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
